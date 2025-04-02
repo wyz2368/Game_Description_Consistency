d840d7af9de62cd08da6ae15e05e5cc80bb5275e
Shilong Deng <55789594+zczlsde@users.noreply.github.com>
2025-04-02 15:21:01 +0100

    Update __init__.py

--- Tree/__init__.py
+++ Tree/__init__.py
@@ -2,5 +2,4 @@ from .tree import Node
 from .tree import NodeType
 from .tree import EFGParser
 from .tree import GameTree
-from .tree import compare_outcomes
 from .tree import compare_chance_probs, compare_information_sets
